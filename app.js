ba78652fa1d2bac9bfa019e855d3f19d2d33c59e
MarioHanzel <48348105+MarioHanzel@users.noreply.github.com>
2019-05-17 17:51:05 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ console.log("Hello world!");
 alert("Hello!");
 
 var target = document.getElementById("target");
+target.innerText = "ji!";
+
+var button = document.getElementById("button");
+button.addEventListener('click',function() {
+  alert("yes thanks for clicking me");
+});
 
